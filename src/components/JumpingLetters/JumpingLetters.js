9edc0a7110fdbd4ddbb2647a5f47e25a7f0c2a09
simonghales <simonghales@gmail.com>
2017-12-06 20:31:22 +1000

    added background tile and styled the loading screen

---
+++ src/components/JumpingLetters/JumpingLetters.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './JumpingLetters.css';
+import classNames from 'classnames';
+
+function getLetters(label: string): string[] {
+  let letters = [];
+  for (let i = 0, len = label.length; i < len; i++) {
+    letters.push(label[i]);
+  }
+  return letters;
+}
+
+const JumpingLetters = ({label = '', speed = 'normal'}) => {
+  return (
+    <div className={classNames([
+      'JumpingLetters',
+      `JumpingLetters--speed-${speed}`,
+    ])}>
+      {
+        getLetters(label).map((letter: string, index) => (
+          <span key={index} className={classNames([
+            {
+              'emptySpace': (letter === ' '),
+            }
+          ])}>{letter}</span>
+        ))
+      }
+    </div>
+  )
+};
+
+export default JumpingLetters;
--- src/screens/AppLoadingScreen/AppLoadingScreen.js
+++ src/screens/AppLoadingScreen/AppLoadingScreen.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import './AppLoadingScreen.css';
 import classNames from 'classnames';
+import JumpingLetters from '../../components/JumpingLetters/JumpingLetters';
 
 const AppLoadingScreen = () => {
   return (
     <div className='AppLoadingScreen'>
       <div className='AppLoadingScreen__content'>
-        <h2 className='AppLoadingScreen__title'>fruit salad</h2>
-        {/*<h3 className='AppLoadingScreen__subtitle'>yummy yummy</h3>*/}
-        {/*<div className='AppLoadingScreen__loading'>loading...</div>*/}
+        <h2 className='AppLoadingScreen__title'>
+          fruit salad
+        </h2>
+        <h3 className='AppLoadingScreen__subtitle'>
+          <JumpingLetters label='nomming...'/>
+        </h3>
       </div>
     </div>
   );
